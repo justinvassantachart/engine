917cd06382f5c60b1e209717de6f9261bde5051d
fabio <fabioi@stanford.edu>
2026-04-02 15:40:26 -0700

    feat: dap initial types
    Signed-off-by: fabio <fabioi@stanford.edu>

--- src/dap/types.rs
+++ src/dap/types.rs
@@ -15,3 +15,36 @@ pub struct Variable {
     pub value: String,
     pub r#type: Option<String>,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ProtocolMessage {
+    pub seq: i64,
+
+    #[serde(flatten)]
+    pub body: MessageBody,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum MessageBody {
+    #[serde(rename = "request")]
+    Request {
+        #[serde(flatten)]
+        command: RequestCommand,
+    },
+    #[serde(rename = "response")]
+    Response {
+        request_seq: i64,
+        success: bool,
+        command: String,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        message: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        body: Option<ResponseBody>,
+    },
+    #[serde(rename = "event")]
+    Event {
+        #[serde(flatten)]
+        body: EventBody,
+    },
+}
