20c2fc68ca64decaf8f940534213882b75cb2dfc
Jacob Roberts-Baca <jacobrobertsbaca@gmail.com>
2026-04-09 18:13:10 -0700

    chore: strip away most of debugger interface, provide only dap

---
+++ src/ts/debugger.ts
@@ -0,0 +1,48 @@
+import EventEmitter from 'events';
+
+import { DapAdapter, LocationInfo as RustLocation } from '../../pkg/runtime';
+import { Internals } from './internals';
+
+export type LocationInfo = Omit<RustLocation, 'file'> & {
+  readonly file: string;
+};
+
+type DebuggerEventMap = {
+  dap: [unknown];
+};
+
+export class Debugger extends EventEmitter<DebuggerEventMap> {
+  /**
+   * Access to internal properties of the debugger.
+   * These are put under a special symbol so that they cannot be accessed by
+   * clients of the library.
+   */
+  [Internals]: {
+    attach(worker: Worker): void;
+  };
+
+  private readonly dap: DapAdapter;
+
+  constructor() {
+    super();
+    this[Internals] = {
+      attach: this.attach.bind(this),
+    };
+
+    this.onMessage = this.onMessage.bind(this);
+    this.dap = new DapAdapter();
+    this.dap.on(this.onMessage);
+  }
+
+  public send(message: unknown) {
+    this.dap.sendMessage(message);
+  }
+
+  private attach(worker: Worker) {
+    this.dap.attach(worker);
+  }
+
+  private onMessage(message: unknown) {
+    this.emit('dap', message);
+  }
+}
