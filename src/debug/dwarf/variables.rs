6f9d85c9622b1f029467f9dd1b2d9a363bfb28e5
fabio <fabioi@stanford.edu>
2026-04-15 12:09:34 -0700

    feat: variable scaffolding
    Signed-off-by: fabio <fabioi@stanford.edu>

--- src/debug/dwarf/variables.rs
+++ src/debug/dwarf/variables.rs
@@ -3,6 +3,7 @@ use crate::{
     types::GlobalAddress,
 };
 use gimli::read::Expression;
+use std::collections::HashMap;
 
 use super::R;
 
@@ -52,7 +53,75 @@ pub fn get_location(die: &Die<'_>, pc: GlobalAddress) -> Option<Expression<R>> {
     die.expression(gimli::DW_AT_location, pc)
 }
 
+type TypeId = gimli::DieReference;
+
 pub struct Value {
     inner: Vec<gimli::Piece<R>>,
-    ty: DieReference,
+    ty: TypeId,
+}
+
+pub struct TypeGraph {
+    types: HashMap<TypeId, TypeDeclaration>,
+}
+
+#[derive(Clone, Debug)]
+pub enum MemberLocation {
+    Offset(i64),
+    Expr(gimli::Expression<R>),
+}
+
+#[derive(Clone, Debug)]
+pub struct StructureMember {
+    pub location: Option<MemberLocation>,
+    pub name: Option<String>,
+    pub ty: TypeId,
+}
+
+#[derive(Clone, Debug)]
+pub enum ReferenceKind {
+    Pointer,
+    Reference,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum Modifier {
+    TypeDef,
+    Const,
+    Volatile,
+    Atomic,
+    Restrict,
+}
+
+#[derive(Clone, Debug)]
+pub enum ArrayBound {
+    Expr(gimli::Expression<R>),
+    Count(i64),
+}
+
+#[derive(Clone, Debug)]
+pub enum TypeDeclaration {
+    Scalar {
+        name: String,
+        byte_size: u64,
+        encoding: gimli::DwAte,
+    },
+    Array {
+        byte_size: Option<u64>,
+        element_type: TypeId,
+        lower_bound: ArrayBound,
+        upper_bound: Option<ArrayBound>,
+    },
+    Referential {
+        target: TypeId,
+        kind: ReferenceKind,
+    },
+    Structure {
+        name: Option<String>,
+        byte_size: u64,
+        members: Vec<StructureMember>,
+    },
+    ModifiedType {
+        modifier: Modifier,
+        inner: TypeId,
+    },
 }
